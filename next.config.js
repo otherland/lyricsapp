43905a412fe4b495539b5bcbadfaa3b59a88b69e
admin <tomdev@protonmail.com>
2023-02-23 15:26:47 +0000

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = (phase, { defaultConfig }) => {
   return {
     ...defaultConfig,
 
+	assetPrefix: isProd ? '/lyricsapp/' : '',
+	images: {
+	unoptimized: true,
+	},
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       config.node = {
         fs: 'empty'
@@ -10,3 +16,4 @@ module.exports = (phase, { defaultConfig }) => {
     },
   }
 }
+
